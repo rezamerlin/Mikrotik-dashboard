1f378e7a974dcc706022d80d6d045f017a428d12
Reza Rahimi <rezarahimi130@gmail.com>
2026-02-13 10:28:58 +0330

    second version

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-const url = "5.202.8.17:1377";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/router": {
-        target: url,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/router/, ""),
-      },
-    },
-  },
 });
